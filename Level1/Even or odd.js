ee00212faeb8508920aaae742b05d9a99b0dea8d
Amr-Musad <153203714+Amr-Musad@users.noreply.github.com>
2023-12-26 16:54:47 +0200

    Update Even or odd.js

--- Level1/Even or odd.js
+++ Level1/Even or odd.js
@@ -1,1 +1,13 @@
+//This program generates a random integer number and tests whether it is even or odd.
 
+
+//Genterating the random integer number
+var number = Math.random();
+number = parseInt((number*10)/1);
+//Testing if the number is odd or even
+if ((number % 2) == 0){
+    console.log(number," is an even number");
+}
+else{
+    console.log(number," is an odd number" );
+}
